569625ebe014f8164b4c7175904cbe1993d8fd22
binforkoreait <seongubabo1@gmail.com>
2026-04-09 16:36:16 +0900

    11

--- GyeongbinMidtermExam/26311013_BaekGyeongBinNumberPyramid/NumberPyramid.c
+++ GyeongbinMidtermExam/26311013_BaekGyeongBinNumberPyramid/NumberPyramid.c
@@ -2,3 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+int array[30];
+
+void Sort(int n);
+
+int main()
+{
+	
+
+
+	return 0;
+}
+
+void Sort(int n)
+{
+	int i, j, temp;
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = 0; j < n - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
+}
